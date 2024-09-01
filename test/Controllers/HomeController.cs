f5e9432041f5032d964179c4a42d1915dfdbd744
2218121021-MertCan-Kabakci <125986530+2218121021-MertCan-Kabakci@users.noreply.github.com>
2024-09-01 19:33:49 +0300

    Add project files.

---
+++ test/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using test.Models;
+
+namespace test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string username, string password, string terms)
+        {
+            if (string.IsNullOrWhiteSpace(terms))
+            {
+                ViewBag.Message = "You must agree to the terms and conditions.";
+                return View();
+            }
+            if (username == "pass" && password == "word")
+            {
+                ViewBag.Message = "Giriş başarılı!";
+                return RedirectToAction("About");
+
+            }
+            else if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                ViewBag.Message = "Username or password is empty";
+                return View();
+            }
+            else
+            {
+                ViewBag.Message = "Kullanıcı adı veya şifre hatalı.";
+                return View();
+            }
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
